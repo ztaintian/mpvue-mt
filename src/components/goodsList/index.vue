a6cac578af647609317916c05806554ec8a31b7a
unknown <zhutiantian@ciicsh.local>
2018-06-29 16:31:17 +0800

    list

--- src/components/goodsList/index.vue
+++ src/components/goodsList/index.vue
@@ -1,86 +1,183 @@
 <template>
   <scroll-view>
-    <!-- <div class="field">                                
-      <a href="/restaurant/452815109836374">                                    
-        <div class="avatar">
-          <span class="avatar-brand">品牌</span>
-          <img src="http://p0.meituan.net/waimaipoi/2adcd76b07b13794a82114c8bd45a2e927062.jpg.webp" data-src-retina="http://p0.meituan.net/waimaipoi/2adcd76b07b13794a82114c8bd45a2e927062.jpg.webp" class="j-poi-pic avatar-img">
-        </div>                                    
-      </a>
-      <div class="content">
-        <a href="/restaurant/452815109836374">
-        <div class="shop-title shop-title-icon-0">                                            
-          <div class="shop-na">满记甜品（上海星游城店）</div>
-        </div>
-        <div class="shop-mid-line clearfix">
-            <div class="appr-status">
-              <i class="appr-score"></i>
-              <i class="appr-score"></i>
-              <i class="appr-score"></i>
-              <i class="appr-score"></i>
-              <i class="appr-score"></i>
-            </div>
-            <div class="shop-sold">月售999+</div>
-            <i class="shop-right">920m</i>
-            <i class="shop-dvd-line">|</i>
-            <i class="shop-delivery-time">30min</i>
-            </div>                                        
-            <div class="shop-line clearfix">                                            
-              <span class="shop-line-item">起送价<em>￥25</em></span>                                            
-              <span class="shop-line-item"><span class="shop-line-item-line">|</span>配送 ¥5.3</span>
-              <div class="allocation-icon"><span>美团专送</span></div>
-              <div class="clear"></div>                                        
-              </div>
-              <div class="j-shop-discount-entries clearfix shop-discount-entries">                                
-                <ul class="shop-discount-entry-wrap-overflow clearfix">
-                  <li class="shop-discount-entry clearfix"><i class="i-x12"><img class="i-x12 bugimg" src="http://p0.meituan.net/xianfu/476ba65ee80b6385bab292c085baed17940.png.webp"></i>                                            <p class="shop-discount-entry-desc shop-discount-overflow">本店支持开发票，开票金额0元起</p>                                        </li>
-                </ul>
-              </div>
-              </a>                                                           
+    <div class="field" v-for="(v,index) in poilist">                                                              
+      <div class="avatar">
+        <span class="avatar-brand">品牌</span>
+        <img class="avatar-image" :src="v.pic_url" />
       </div>
-    </div> -->
+      <div class="content">                                         
+        <div class="shop-na">{{v.name}}</div>
+        <div class="clearfix" style="width:100%;height:44rpx;">
+          <div class="shop-star">
+            <v-star :score="v.wm_poi_score" :size="size" ></v-star>
+          </div>
+          <div class="shop-sold">月售{{v.month_sale_num > 999?'999+':v.month_sale_num}}</div>
+          <i class="shop-right">{{v.distance}}</i><i class="shop-dvd-line">|</i><i class="shop-delivery-time">{{v.avg_delivery_time}}min</i>
+        </div>
+        <div class="shop-line clearfix">                                            
+          <span class="shop-line-item">{{v.min_price_tip}}</span>                                            
+          <span class="shop-line-item"><span class="shop-line-item-line">|</span>{{v.shipping_fee_tip}}</span>
+          <div class="allocation-icon"><span>美团专送</span></div>
+          <div class="clear"></div>                                        
+        </div>      
+      </div>                                    
+      <!-- <v-star :score="score"></v-star> -->
+    </div>
+    <div class="bottom-load" v-if="bottomFlag">加载中...</div>
   </scroll-view>
 </template>
 
 <script>
+import star from '../star'
 export default {
+  components: {
+    'v-star': star
+  },
   data () {
     return {
+      score: 3,
+      bottomFlag: false,
+      size: 24,
+      poilist: []
     }
   },
   mounted () {
+    this.ajaxList()
   },
   methods: {
+    ajaxList () {
+      this.$http.get('/list').then((data) => {
+        this.pageList = data.data.poilist
+        this.poilist = data.data.poilist.splice(0, 10)
+      })
+    }
   },
   onReachBottom () {
+    wx.showLoading({
+      title: '加载中'
+    })
+    setTimeout(() => {
+      this.poilist.push(...this.pageList)
+      wx.hideLoading()
+    }, 1000)
   }
 }
 </script>
 
 <style lang='stylus' scoped>
-.categoryList {
-  width: 100%;
-  height: 350rpx;
-  border-bottom: 20rpx solid #f4f4f4;
-
-  .category-info {
-    display: inline-block;
-    text-align: center;
-    position: relative;
-    margin-top: 10rpx;
-    width: 25%;
-    height: 150rpx;
-
-    image {
-      width: 95rpx;
-      height: 95rpx;
-    }
-
-    .category-text {
-      font-size: 25rpx;
-      width: 100%;
-      line-height: 30rpx;
-    }
-  }
-}
+.field 
+  display flex
+  margin-left 20rpx
+  padding: 30rpx 0 25rpx;
+  border-bottom 1rpx solid #E4E4E4
+  &::after
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 2rpx;
+    background: #E4E4E4;
+    bottom: 0;
+    -webkit-transform: scaleY(0.5);
+    -moz-transform: scaleY(0.5);
+    -ms-transform: scaleY(0.5);
+    -o-transform: scaleY(0.5);
+    transform: scaleY(0.5);
+  .avatar 
+    position relative
+    .avatar-brand 
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 56rpx;
+      font-size 20rpx
+      line-height: 28rpx;
+      height: 28rpx;
+      color: #fff;
+      background: #FFA627;
+      text-align: center;
+    .avatar-image
+      width 168rpx
+      height 126rpx
+      border 1rpx solid #eee
+  .content
+    margin-left 10rpx
+    flex 1
+    .shop-na 
+      font-size: 30rpx;
+      height:38rpx;
+      color: #333;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+      font-weight: 500;
+    .shop-star
+      float: left;
+    .shop-sold 
+      float: left;
+      margin-top: 16rpx;
+      font-size: 22rpx;
+      color: #656565;
+    .shop-right 
+      float: right;
+      margin-right: 15px;
+      font-size: 22rpx;
+      padding-top: 16rpx;
+      color: #656565;
+      text-align: right;
+    .shop-dvd-line
+      float: right;
+      margin:23rpx 10rpx 0 ;
+      width: 0;
+      height: 20rpx;
+      font-size: 0;
+      border-right: 1px solid #ccc;
+    .shop-delivery-time 
+      float: right;
+      font-size: 22rpx;
+      margin-top: 16rpx;
+      color: #656565;
+      text-align: right;
+    .shop-line 
+      clear: both;
+      font-size: 22rpx;
+      color: #656565;
+      width 100%
+      .shop-line-item
+        float left
+      .shop-line-item-line
+        margin:23rpx 10rpx 0 ;
+        color #ccc
+      .allocation-icon 
+        float: right;
+        margin-right: 15px;
+        margin-top: 0;
+      .allocation-icon > span 
+        position: relative;
+        background-color: #FFD161;
+        display: inline-block;
+        height: 15px;
+        line-height: 15px;
+        padding: 0 6px;
+        font-size: 10px;
+        color: #333;
+        font-weight: normal;
+        vertical-align: baseline;
+      .allocation-icon > span:before, .allocation-icon > span:after 
+        content: '';
+        position: absolute;
+        border: 2px solid #fff;
+        width: 0;
+        height: 0;
+        font-size: 0;
+      .allocation-icon > span:before 
+        left: 0;
+        top: 0;
+        border-color: #fff #FFD161 #FFD161 #fff;
+      .allocation-icon > span:after
+        right: 0;
+        bottom: 0;        
+        border-color: #FFD161 #fff #fff #FFD161;
+.bottom-load
+  text-align center
+  font-size 22rpx
 </style>
---
+++ src/components/star/index.vue
@@ -0,0 +1,97 @@
+<template>
+  <div :class="[starType,'star']" >
+    <span :class="[itemType,'star-item']" v-for="(itemType,index) in itemTypes" :key="index"></span>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      length: 5,
+      on: 'on',
+      half: 'half',
+      off: 'off'
+    }
+  },
+  props: {
+    size: {
+      type: Number,
+      default: 24
+    },
+    score: {
+      type: Number,
+      default: 4.5
+    }
+  },
+  computed: {
+    starType () {
+      return `star-${this.size}`
+    },
+    itemTypes () {
+      var result = []
+      var intScore = Math.floor(this.score)
+      var hasDecimal = this.score % 1 !== 0
+      for (var i = 0; i < intScore; i++) {
+        result.push(this.on)
+      }
+      if (hasDecimal) {
+        result.push(this.half)
+      }
+      while (result.length < this.length) {
+        result.push(this.off)
+      }
+      return result
+    }
+  },
+  mounted () {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang='stylus' scoped>
+.star
+  .star-item
+    font-size 0
+    display inline-block
+    background-repeat no-repeat
+  &.star-48
+    .star-item
+      width 40rpx
+      height 40rpx
+      margin-right 44rpx
+      background-size 40rpx 40rpx
+      &.on
+        bg-image("star48_on")
+      &.half
+        bg-image("star48_half")
+      &.off
+        bg-image("star48_off")
+  &.star-36
+    .star-item
+      width 30rpx
+      height 30rpx
+      margin-right 30rpx
+      background-size 30rpx 30rpx
+      &.on
+        bg-image("star36_on")
+      &.half
+        bg-image("star36_half")
+      &.off
+        bg-image("star36_off")
+  &.star-24
+    .star-item
+      width 20rpx
+      height 20rpx
+      margin-right 10rpx
+      background-size 20rpx 20rpx
+      &.on
+        bg-image("star24_on")
+      &.half
+        bg-image("star24_half")
+      &.off
+        bg-image("star24_off")
+
+</style>
